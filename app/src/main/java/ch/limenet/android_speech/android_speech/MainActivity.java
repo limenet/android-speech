81f12200bae2f652ba5ab798a218e18ef3b3f73a
Linus Metzler <linus.metzler@gmail.com>
2017-05-27 22:41:26 +0200

    Revert "some refactoring"
    This reverts commit 0cef13ee4e638857b3118b89f160aa4db0992faf.

--- app/src/main/java/ch/limenet/android_speech/android_speech/MainActivity.java
+++ app/src/main/java/ch/limenet/android_speech/android_speech/MainActivity.java
@@ -1,5 +1,6 @@
 package ch.limenet.android_speech.android_speech;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -11,15 +12,19 @@ import java.util.HashMap;
 import java.util.Locale;
 
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Map;
 
 import android.net.wifi.WifiManager;
 
+import fi.iki.elonen.NanoHTTPD;
+
 public class MainActivity extends AppCompatActivity {
-    public static final int PORT = 8765;
-    private Server server;
+    private static final int PORT = 8765;
+    private TtsHTTP server;
     private WifiManager.WifiLock wifiLock;
     private HashMap<String, TextToSpeech> ttsEngines;
 
@@ -35,19 +40,21 @@ public class MainActivity extends AppCompatActivity {
         int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
         final String formattedIpAddress = String.format(Locale.US, "%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff),
                 (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+        setTextStatus("Please access http://" + formattedIpAddress + ":" + PORT);
 
         try {
-            server = new Server(this);
+            server = new TtsHTTP();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         ttsEngines = new HashMap<>();
-        ttsEngines.put("en", new LocalizedTTS(Locale.US, this.getApplicationContext()).tts);
-        ttsEngines.put("de", new LocalizedTTS(Locale.GERMAN, this.getApplicationContext()).tts);
+        ttsEngines.put("en", new TtsLocale(Locale.US, this.getApplicationContext()).tts);
+        ttsEngines.put("de", new TtsLocale(Locale.GERMAN, this.getApplicationContext()).tts);
+
     }
 
-    public void speak(String loc, String text) {
+    private void speak(String loc, String text) {
         setTextOutputLanguage(loc);
         setTextOutputText(text);
         if (Build.VERSION.SDK_INT >= 21) {
@@ -55,6 +62,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             ttsEngines.get(loc).speak(text, TextToSpeech.QUEUE_ADD, null);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
@@ -77,6 +94,16 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
     public void setTextStatus(final String t) {
         TextView tv = (TextView) findViewById(R.id.status);
         tv.setText(t);
@@ -96,6 +123,44 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://limenet.ch"));
         startActivity(intent);
+    }
+
+
+    private class TtsHTTP extends NanoHTTPD {
+        TtsHTTP() throws IOException {
+            super(PORT);
+            start();
+        }
+
+        @Override
+        public Response serve(IHTTPSession session) {
+            try {
+                session.parseBody(new HashMap<String, String>());
+            } catch (IOException | ResponseException e) {
+                e.printStackTrace();
+            }
+            Map<String, String> params = session.getParms();
+            String pText = params.get("text");
+            String pLoc = params.get("locale");
+            speak(pLoc, pText);
+            return new NanoHTTPD.Response(Response.Status.OK, "text/plain", pText);
+        }
+    }
+
+    private class TtsLocale implements OnInitListener {
+        private Locale loc;
+        TextToSpeech tts;
+
+        TtsLocale(Locale loc, Context context) {
+            this.loc = loc;
+            tts = new TextToSpeech(context, this);
+        }
+
+        public void onInit(int initStatus) {
+            if (initStatus == TextToSpeech.SUCCESS) {
+                tts.setLanguage(loc);
+            }
+        }
     }
 
 }
